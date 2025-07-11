7893c907b8317f3601dff1d52e9cb58e96e9a054
Zlash65 <zarrar65@gmail.com>
2025-07-11 18:27:28 +0530

    feat: add clerk for user auth and management

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,21 @@
 import * as React from "react"
 
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
 const Header = () => {
-  return <div className="text-center">Header</div>
+  return <div className="text-center">
+                <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+  </div>
 };
 
 export default Header;
