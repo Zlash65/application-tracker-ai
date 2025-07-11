ddaaad6b2d56f9fbbfe815f5f208fc4e4ed3065d
Zlash65 <zarrar65@gmail.com>
2025-07-12 01:45:43 +0530

    feat: init inngest and prisma

--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Work in progress...
+    <div>
+      <div className="grid-background">
+        Work in progress...
+      </div>
     </div>
   );
 }
