63ff69c8d7d8ec6e8b49abe4efadfa8fc6cd203f
Zlash65 <zarrar65@gmail.com>
2025-07-12 17:08:05 +0530

    feat: add onboarding page and update user api

---
+++ data/industries.js
@@ -0,0 +1,16 @@
+export const industries = [
+  {
+    id: "tech",
+    name: "Technology",
+    subIndustries: [
+      "Software Development",
+      "IT Services",
+      "Cybersecurity",
+      "Cloud Computing",
+      "AI / ML",
+      "Data Science & Analytics",
+      "Internet & Web Services",
+      "Blockchain & Cryptocurrency"
+    ]
+  }
+];
